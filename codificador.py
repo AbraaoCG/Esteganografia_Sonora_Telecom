f126dcd088c98b7d0dcf3862e8c1fc4acd388c8e
abraao-PC-Win <abraao.cg@poli.ufrj.br>
2023-10-17 18:43:53 -0300

    Codificador aprimorado e decodificador implementado

--- codificador.py
+++ codificador.py
@@ -5,16 +5,16 @@ from scipy.io import wavfile
 # Recebe um caractere como entrada e calcula a frequência 
 # correspondente com base na distância entre o caractere de primeira numeração --> chr(1).
 # no alfabeto
-def calcular_frequencia(char):
+def calcular_frequencia(char,difFreq):
     if(ord(char) > 255):
         print('Erro: mensagem deve ser utilizar caracteres na formatação Unicode')
-    return frequencia_inicial + difFreq * (ord(char) - 1)
+    return frequencia_inicial + difFreq * (ord(char))
 
 
 # Argumento para plot ou não do gráfico esperado no espectrometro
 plotFlag = False
 
-# Frequência inicial para o caractere inicial unicode
+# Frequência inicial para o caractere inicial unicode, frequencia máxima e número de caracteres representados.
 frequencia_inicial = 50
 freq_maxima = 20000
 numCaracteresMax = 256
@@ -27,17 +27,15 @@ entrada = input("Digite uma mensagem: ")
 
 # Duração de cada caractere em segundos
 duracao_caractere = 0.5
+
 # Taxa de amostragem fixa em 'ta' Hz ('ta' amostras por segundo)
 ta = 44100
 
 # Lista para armazenar as frequências de cada caractere
-frequencias = [calcular_frequencia(char) for char in entrada]
-print(frequencias)
-frequencias.append(frequencias[-1]+duracao_caractere)
+frequencias = [calcular_frequencia(char,difFreq) for char in entrada]
 
 # Lista para armazenar o tempo de início de cada caractere
 tempos_inicio = [i * duracao_caractere for i in range(len(entrada))]
-#tempos_inicio.append(tempos_inicio[-1]+duracao_caractere)
 
 # Cada caractere deve ter duração de 0.5 s.
 # num_amostras = D x S
@@ -48,6 +46,7 @@ num_amostras = int(duracao_caractere * ta)
 # um caractere (duracao_caractere), com ( duracao_caractere * ta ) amostras por caractere.
 tempo = np.linspace(0, duracao_caractere, num_amostras, endpoint=False)
 
+
 # Agora ocorre a geração do sinal de áudio.
 
 # Para cada caractere, geramos um sinal senoidal com frequência
@@ -55,6 +54,7 @@ tempo = np.linspace(0, duracao_caractere, num_amostras, endpoint=False)
 # para gerar um sinal final composto por N senoides de frequências
 # distintas.
 sinal = np.concatenate([np.sin(2 * np.pi * f * tempo) for f in frequencias])
+
 
 # Por fim, salvamos o sinal em um arquivo WAV, definindo a taxa de
 # amostragem como ta Hz e converte o sinal para o formato float32.
---
+++ decodificador0.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+# Função para remontar mensagem baseado nas frequências ordenadas.
+def decodificar_frequencias(list_freq):
+    
+    # Frequência inicial para o caractere inicial unicode, frequencia máxima e número de caracteres representados.
+    frequencia_inicial = 50
+    freq_maxima = 20000
+    numCaracteresMax = 256
+    freqRange = freq_maxima - frequencia_inicial
+    difFreq = freqRange / numCaracteresMax # Diferença de frequência entre dois caracteres.
+
+    msg_decodificada = []        
+    for f in list_freq:
+            posicao = int( round( (f - frequencia_inicial) / difFreq) )
+            char = chr(posicao)
+            msg_decodificada.append(char)
+    return ''.join(msg_decodificada)
+
+
+def decodificar_mensagem(filename = 'message.wav'):
+    # Carregue o arquivo de áudio
+    sample_rate, audio_data = wavfile.read(filename)
+
+    # Defina o tamanho do segmento( 1 caractere ) em segundos (0.5 segundos)
+    segment_duration = 0.5
+    segment_samples = int(segment_duration * sample_rate)
+
+
+    lista_frequencias = []
+    # Para cada segmento, calcule a frequência dominante utilizando FFT.
+    for i in range(0, len(audio_data) - 1, segment_samples):
+        segment = audio_data[i:i+segment_samples]
+        # Use a Transformada de Fourier para calcular as frequências
+        frequencies = np.fft.fftfreq(len(segment), 1 / sample_rate)
+        #print(len(frequencies))
+        magnitudes = np.abs(np.fft.fft(segment))
+        
+        # Encontre a frequência dominante no segmento
+        dominant_frequency = np.abs(frequencies[np.argmax(magnitudes)]) # Frequência é espelhada, então se aparecer uma frequência negativa na verdade a gerada foi positiva.
+        lista_frequencias.append(dominant_frequency)
+
+    mensagem = decodificar_frequencias(lista_frequencias)
+
+
